4bde4391ab685193d78adaf4be40921b3bf0b1d8
songini021715 <83641847+songini021715@users.noreply.github.com>
2021-11-23 20:20:51 +0800

    Add files via upload

---
+++ test1/src/test/java/MyTestSuite/AllTest.java
@@ -0,0 +1,25 @@
+package MyTestSuite;
+
+import junit.framework.JUnit4TestAdapter;
+import junit.framework.TestSuite;
+import calculate.CalculatorTest;
+import MyMathArrays.MyMathArraysTest;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({CalculatorTest.class, MyMathArraysTest.class})
+public class AllTest {
+    public static Test suite(){
+        TestSuite suite = new TestSuite("All JUnit test");
+        suite.addTest(new JUnit4TestAdapter(MyMathArraysTest.class));
+        suite.addTest(new JUnit4TestAdapter(CalculatorTest.class));
+        return (Test) suite;
+    }
+//    public void testsuite(){
+//        System.out.println("开始运行测试集");
+//    }
+}
+
